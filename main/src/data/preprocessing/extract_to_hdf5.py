67383595814a4c137e287e867027ad9f98e4c373
Rob174 <robin.128.456@gmail.com>
2021-06-04 13:27:00 +0100

    Support save datetime, id, and preprocessing

--- main/src/data/preprocessing/extract_to_hdf5.py
+++ main/src/data/preprocessing/extract_to_hdf5.py
@@ -1,5 +1,4 @@
 
-
 # Extract file names under the following structure
 
 # {"shp":{"id":fullpath,...}, "img":, {"id":fullpath,...}}
@@ -7,17 +6,24 @@
 import os, re
 from main.FolderInfos import FolderInfos
 FolderInfos.init(test_without_data=True)
-shapefile_path = FolderInfos.input_data_folder+"originals"+FolderInfos.separator+"Hydrocarbures_liquides_Seeps_et_spills_WGS84.shp"
-dbffile_path = FolderInfos.input_data_folder+"originals"+FolderInfos.separator+"Hydrocarbures_liquides_Seeps_et_spills_WGS84.dbf"
-images_path_folder = FolderInfos.input_data_folder+"originals"+FolderInfos.separator+FolderInfos.separator.join("Sentinel1\\TraitementSnap".split("\\")) + FolderInfos.separator
+shapefile_path = FolderInfos.input_data_folder +"originals" +FolderInfos.separator +"Hydrocarbures_liquides_Seeps_et_spills_WGS84.shp"
+dbffile_path = FolderInfos.input_data_folder +"originals" +FolderInfos.separator +"Hydrocarbures_liquides_Seeps_et_spills_WGS84.dbf"
+images_path_folder = FolderInfos.input_data_folder +"originals" +FolderInfos.separator +FolderInfos.separator.join \
+    ("Sentinel1\\TraitementSnap".split("\\")) + FolderInfos.separator
 
-dico_by_extensions = {"img":{}}
+dico_by_extensions = {"img" :{}}
+dico_infos_img = {}
 
 for folder in os.listdir(images_path_folder):
-    img_file_path = images_path_folder+folder+FolderInfos.separator+"Sigma0_VV_db.img"
+    img_file_path = images_path_folder +folder +FolderInfos.separator +"Sigma0_VV_db.img"
     # Extract the name of the file
-    name = re.sub("^([A-Za-z0-9_]+[A-Za-z_]{,2})(20[0-9_A-Z]+)(\\.[a-z]+)$","\\2",img_file_path.split(FolderInfos.separator)[-2])
-    dico_by_extensions["img"][name] = img_file_path
+    [_ ,t_init ,t_end ,uniq_id ,preprocessing] = re.sub(
+        "^(([0-9A-Za-z]+_){4})(\\d{8}T\\d{6})_(\\d{8}T\\d{6})_(([0-9A-Za-z]+_[0-9A-Za-z]+_[0-9A-Za-z]+))_([^\\.]+)\\.data$",
+        "\\1,\\3,\\4,\\5,\\7",
+        folder
+        ).split(",")
+    dico_by_extensions["img"][uniq_id] = img_file_path
+    dico_infos_img[uniq_id] = {"t_init" :t_init ,"t_end" :t_end ,"uniq_id" :uniq_id ,"preprocessing" :preprocessing}
 
 # Write the following files:
 
@@ -30,6 +36,8 @@ The value 3 correspond respectively (in this order) to oil seep, oil spill and o
 - images_informations.json: will store
     - coord_upperleft and coord_lowerright: floats tuples. Coordinates of the upper left and lower right corners in degrees
     - resolution: floats tuple: Its resolution in px/meters
+    - tinit,tend: initial and ending time when satelite start to capture
+    - preprocessing: code of preprocessing steps
 
 All data will be accessible by calling object[image_id]
 """
@@ -41,15 +49,18 @@ import json
 FolderInfos.init(test_without_data=True)
 
 ## Create objects hdf5 and container for th images informations
-images_hdf5 = File(f"{FolderInfos.input_data_folder}images_preprocessed.hdf5","w")
-annotations_labels_hdf5 = File(f"{FolderInfos.input_data_folder}annotations_labels_preprocessed.hdf5","w")
+with File(f"{FolderInfos.input_data_folder}images_preprocessed.hdf5" ,"r") as images_hdf5:
+    already_done = images_hdf5.keys()
+
+images_hdf5 = File(f"{FolderInfos.input_data_folder}images_preprocessed.hdf5" ,"w")
+annotations_labels_hdf5 = File(f"{FolderInfos.input_data_folder}annotations_labels_preprocessed.hdf5" ,"w")
 images_informations = {}
 
 import geopandas as gpd
 from shapely import speedups
 import rasterio
 import numpy as np
-from PIL import Image,ImageDraw # PIL = pillow
+from PIL import Image ,ImageDraw # PIL = pillow
 import dbf
 
 speedups.disable() # To avoid errors
@@ -61,62 +72,70 @@ table = dbf.Table(dbffile_path)  # Table containing the class and the index of t
 table.open()
 ## Loop through images, open them and add their informations to the correspinding objects
 nb_elems = len(dico_by_extensions["img"])
-for i,[name,pathImg] in enumerate(dico_by_extensions["img"].items()):
-    print(f"Progress {(i+1)/nb_elems}%")
+for i ,[name ,pathImg] in enumerate(dico_by_extensions["img"].items()):
+    print(f"Progress {( i +1 ) /nb_elems}%")
+    if name in already_done:
+        continue # We skip this image
     # We loop through raster images and shapefiles
     ## Open the raster
     with rasterio.open(pathImg) as raster: ## (NB: with keyword allows to manage files (properly open and close them)
         image_array: np.ndarray = raster.read(1) # Get the image array
         # Then, we create a "dataset" to be able to access the data by calling hdf5_object[name]. It is not a real dataset as we commonly think as only one image is in it.
-        images_hdf5.create_dataset(name,shape=image_array.shape,dtype='f',
+        images_hdf5.create_dataset(name ,shape=image_array.shape ,dtype='f',
                                    data=image_array)
         # Properties computation
         ## Resolution computation
         # From https://gis.stackexchange.com/questions/311063/extract-raster-information-using-python
         radius_earth_meters = 6371e3
         # raster.transform is the transformation matrix between the spatial coordinates and the pixels # https://en.wikipedia.org/wiki/Transformation_matrix#:~:text=eigenbases-,examples%20in%202%20dimensions,-edit
-        xres: float = abs(raster.transform.a * np.pi / 180. * radius_earth_meters) # We consider that the earth is a sphere
-        yres: float = abs(raster.transform.e * np.pi / 180. * radius_earth_meters) # abs in case of a reflexion transformation
+        xres: float = abs \
+            (raster.transform.a * np.pi / 180. * radius_earth_meters) # We consider that the earth is a sphere
+        yres: float = abs \
+            (raster.transform.e * np.pi / 180. * radius_earth_meters) # abs in case of a reflexion transformation
         ## Coordinates
-        coord_upperleft: Tuple[float,float] = raster.transform * (0, 0)
-        coord_lowerright: Tuple[float,float] = raster.transform * (raster.width, raster.height)
-        images_informations[name] = {"resolution":(xres,yres),"coord_upperleft":coord_upperleft,"coord_lowerright":coord_lowerright}
+        coord_upperleft: Tuple[float ,float] = raster.transform * (0, 0)
+        coord_lowerright: Tuple[float ,float] = raster.transform * (raster.width, raster.height)
+        images_informations[name] = {"resolution" :(xres ,yres) ,"coord_upperleft" :coord_upperleft
+                                     ,"coord_lowerright" :coord_lowerright,
+                                     **dico_infos_img} # **dictionary = dictionnary unpacking: we add the content of the dict to the other dict
 
     # Shapefile segmentation map computation
     ## Create empty array with the same shape as the original image
-    segmentation_map = np.zeros(shape=image_array.shape,dtype=np.uint8)
+    segmentation_map = np.zeros(shape=image_array.shape ,dtype=np.uint8)
     segmentation_map = Image.fromarray(segmentation_map)
     draw = ImageDraw.ImageDraw(segmentation_map)  # draw the base image
-    for i_shape,shape in enumerate(shapefile.geometry):
-        liste_points_shape: List[Tuple[int,int]] = [] # will contain the list of point of this shape
+    for i_shape ,shape in enumerate(shapefile.geometry):
+        liste_points_shape: List[Tuple[int ,int]] = [] # will contain the list of point of this shape
         elem = shape.boundary # extract the boundary of the object shape (with other properties)
-        if elem.geom_type != "LineString":# the polygon is defined by a group of lines defines the polygon : https://help.arcgis.com/en/geodatabase/10.0/sdk/arcsde/concepts/geometry/shapes/types.htm
+        if elem.geom_type != "LineString"  :# the polygon is defined by a group of lines defines the polygon : https://help.arcgis.com/en/geodatabase/10.0/sdk/arcsde/concepts/geometry/shapes/types.htm
             # Utiliser le numéro de vertice pr éviter les croisements
             for line in elem: # Loop through lines of the "Multi" - LineString
                 coords = np.dstack(line.coords.xy).tolist()[0] # get the list of points
                 for point in coords: # Extract the point of the polygon
-                    px, py = raster.index(point[0], point[1]) # Convert point coordinates from degrees to corresponding px coordinates
+                    px, py = raster.index(point[0], point
+                        [1]) # Convert point coordinates from degrees to corresponding px coordinates
                     liste_points_shape.append(tuple([int(px), int(py)]))
 
         else: # the polygon is defined one line which creates a closed shape
             coords = np.dstack(elem.coords.xy).tolist()[0]
             for point in coords: # Extract the point of the polygon
-                px, py = raster.index(point[0], point[1]) # Convert point coordinates from degrees to corresponding px coordinates
+                # Convert point coordinates from degrees to corresponding px coordinates
+                px, py = raster.index(point[0], point[1])
                 liste_points_shape.append(tuple([int(py), int(px)]))
         id_shape = shapefile.id[i_shape]
         metadata = list(filter(lambda x: x[0] == id_shape, table))[0]
         label = metadata[4].strip() # strip cut all space, back to line
-        if label == "seep":
+        if label == "seep":# Change color and so the value put in the array to create the label
             color  = "#010101"
         elif label == "spill":
             color  = "#020202"
         else:
             color = "#000000"
         draw.polygon(liste_points_shape, fill=color)
-    segmentation_map = np.array(segmentation_map,dtype=np.uint8)
-    annotations_labels_hdf5.create_dataset(name,shape=segmentation_map.shape,dtype='i',data=segmentation_map)
+    segmentation_map = np.array(segmentation_map ,dtype=np.uint8)
+    annotations_labels_hdf5.create_dataset(name ,shape=segmentation_map.shape ,dtype='i' ,data=segmentation_map)
 
 # Write the image informations to the corresponding file
 with open(f"{FolderInfos.input_data_folder}images_informations_preprocessed.json") as fp: # NB: fp = filepointer
-    json.dump(images_informations,fp)
+    json.dump(images_informations ,fp)
 table.close()
