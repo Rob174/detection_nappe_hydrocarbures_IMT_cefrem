ca43093d48de2627edae7ac60d186f7b519f0f55
Rob174 <robin.128.456@gmail.com>
2021-06-21 18:13:43 +0100

    :pencil2: correct last commit

--- main/src/data/balance_classes/balance_classes.py
+++ main/src/data/balance_classes/balance_classes.py
@@ -21,7 +21,7 @@ class BalanceClasses1(BaseClass):
         @param classification_label: label with ones of a class is on the image and 0 if not. !! Must be provided by ClassificationPatch make_classification_label method for the shape of the labels (with full details)
         @return: bool, tell if the sample is accepted or rejected
         """
-        tmp_if_accepted_dico = {k:v for k,v in self.attr_prct_accepted}
+        tmp_if_accepted_dico = {k:v for k,v in self.attr_prct_accepted.items()}
         for k in self.attr_number_of_classes:
             self.attr_number_of_classes[k] += int(classification_label[k])
             tmp_if_accepted_dico[k] += int(classification_label[k])
