eb5eceae08b5486901ff8dff06b0f95dc31acd9c
Rob174 <robin.128.456@gmail.com>
2021-06-22 14:18:48 +0100

    analysis.ipynb and tools.py doc

--- main/src/analysis/tools.py
+++ main/src/analysis/tools.py
@@ -9,26 +9,28 @@ from main.FolderInfos import FolderInfos
 from main.src.data.DatasetFactory import DatasetFactory
 
 
-def moving_mean(x,window):
-    new_values = []
-    for i in range(len(x)-window):
-        new_values.append(np.mean(x[i:min(i+window,len(x))]))
-    return new_values
 
 class RGB_Overlay_Patch:
-    def __init__(self,dataset_name="classificationpatch",usage_type="classification",patch_creator="fixed_px",grid_size=1000,input_size=256,classes_to_use="other,seep,spill"):
+    def __init__(self,dataset_name="classificationpatch",usage_type="classification",patch_creator="fixed_px",
+                 grid_size=1000,input_size=256,classes_to_use="other,seep,spill"):
         FolderInfos.init(test_without_data=False)
         self.dataset = DatasetFactory(dataset_name=dataset_name, usage_type=usage_type, patch_creator=patch_creator,
                                       grid_size=grid_size , input_size=input_size,classes_to_use=classes_to_use)
 
-    def __call__(self,name_img,model,blending_factor=0.5,device=None,resize_factor=4):
-        original_img = self.dataset.attr_dataset.images[name_img]
-        transformation_matrix = np.array([[1/resize_factor,0,0],
-                                          [0,1/resize_factor,0],
-                                          [0,0,1]])
-        overlay_true = np.zeros((*original_img.shape,3),dtype=np.float32)
+    def __call__(self,name_img,model,blending_factor=0.5,device=None):
+        """
+        In this function we will constitute patch after patch the overlay of the image filling true and prediction empty matrices with corresponding patches
+        @param name_img: id / name of the image as shown in images_informations_preprocessed.json (better to vizualize but the "official" keys are in the images_preprocessed.hdf5 file)
+        @param model: a created model with pretrained weights already loaded ⚠️
+        @param blending_factor: the percentage (∈ [0.,1.]) of importance given to the color
+        @param device: a pytorch device (gpu)
+        @return: tuple, with  overlay_true,overlay_pred,original_img as np arrays
+        """
+        original_img = self.dataset.attr_dataset.images[name_img] # radar image input (1 channel only)
+        overlay_true = np.zeros((*original_img.shape,3),dtype=np.float32) # prepare the overlays with 3 channels for colors
         overlay_pred = np.zeros((*original_img.shape,3),dtype=np.float32)
         normalize = lambda x:(x-np.min(original_img))/(np.max(original_img)-np.min(original_img))
+        # get the list of patches organized as follows [[input_patch0,output_patch0,filter_patch0],....
         patches = self.dataset.attr_dataset.make_patches_of_image(name_img)
 
         progress = Progress(
@@ -39,22 +41,26 @@ class RGB_Overlay_Patch:
             TimeElapsedColumn(),
             "•",
             TimeRemainingColumn()
-        )
-        with progress:
+        ) # prepare the progress bar -> tell which columns to display
+        with progress: # progress bar manager to use the progress bar
             progression = progress.add_task("generation", name="[red]Progress", total=len(patches))
             for id,[input,output,filter] in enumerate(patches):
-                if filter is False:
+                if filter is True: # skip the image if the dataset ask to skip this patch (can be for multiple reasons -> see DatasetFactory parameters supplied)
                     continue
-                input_adapted = np.stack((input,input,input),axis=0)
+                input_adapted = np.stack((input,input,input),axis=0) # convert patch to rgb
+                # pytorch can only make predictions for batches of images. That is why we create a "batch" of one image by adding one dimension to the image:
+                #shape : (1, img_width, img_height, 3)
                 input_adapted = input_adapted.reshape((1,*input_adapted.shape))
+                # predict output with cpu if no device (gpu) is provided else predict with gpu and ransfer the result on cpu
                 prediction = model(input) if device is None else model(torch.tensor(input_adapted).to(device)).cpu()
+                # get the pixel position of the patch
                 pos_x,pos_y = self.dataset.attr_patch_creator.get_position_patch(id,original_img.shape)
                 if len(output) > 3:
                     raise Exception(f"{len(output)} classes : Too much : cannot use this vizualization technic")
                 input = normalize(input)
-                resized_grid_size = 1000#int(transformation_matrix.dot(np.array([self.dataset.attr_patch_creator.attr_grid_size_px,0,1]))[0])
-                # input = cv2.resize(input,dsize=(resized_grid_size,resized_grid_size), interpolation=cv2.INTER_CUBIC)
-                input = np.stack((input,)*3,axis=-1) # convert in rgb
+                resized_grid_size = self.dataset.attr_patch_creator.attr_grid_size_px
+                input = np.stack((input,)*3,axis=-1) # convert in rgb. NB: (input,)*3 <=> (input,input,input)
+                # initialize the overlay for the patch
                 color_true = np.ones((resized_grid_size,resized_grid_size,3))
                 color_pred = np.ones((resized_grid_size,resized_grid_size,3))
                 for i,c in enumerate(output):
@@ -101,13 +107,14 @@ if __name__ == "__main__":
 
     name = "027481_0319CB_0EB7"
 
-    device = torch.device("cuda")
-    model = ModelFactory(model_name=dico["model"]["attr_model_name"], num_classes=dico["model"]["attr_num_classes"])()
-    model.to(device)
-    model.load_state_dict(torch.load(f"{folder}{choice_folder1}_model_epoch-{epoch}_it-{iteration}.pt"))
-    array_overlay = rgb_overlay(name_img=name, model=model, blending_factor=0.5, device=device)
+    device = torch.device("cuda") # get the gpu
+    model = ModelFactory(model_name=dico["model"]["attr_model_name"], num_classes=dico["model"]["attr_num_classes"])() # create and get the model
+    model.to(device) # put it on the gpu
+    model.load_state_dict(torch.load(f"{folder}{choice_folder1}_model_epoch-{epoch}_it-{iteration}.pt")) # load pretrained weights
+    array_overlay = rgb_overlay(name_img=name, model=model, blending_factor=0.5, device=device) # create the overlay (use method __call__)
 
     import matplotlib.pyplot as plt
+    # show the result in two separated figures
     plt.figure(figsize=(10,10))
     plt.imshow(array_overlay[0])
     plt.figure(figsize=(10,10))
