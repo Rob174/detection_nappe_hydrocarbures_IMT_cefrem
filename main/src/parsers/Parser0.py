24a9fc375da21f4b04ee7d108b9700564b9e2144
Rob174 <robin.128.456@gmail.com>
2021-06-22 11:23:32 +0100

    Adding analysis visualization tools Changing for 10 epochs training

--- main/src/parsers/Parser0.py
+++ main/src/parsers/Parser0.py
@@ -25,7 +25,7 @@ class Parser0(BaseClass):
                     '-model':['model',"resnet18",str,"To choose the network architecture used {"],
                     '-classes':['classes',"seep,spill",str,"Indicate the class used for training separated by an underscore"],
                     # Training
-                    '-num_epochs':['num_epochs',1,int,"Number of epochs / repetitions of the training dataset"],
+                    '-num_epochs':['num_epochs',10,int,"Number of epochs / repetitions of the training dataset"],
                     '-eval_step':['eval_step',10,int,"Number of training steps between two evaluation/validation steps"],
                     '-loss':['loss_preference',"binarycrossentropy",str,"Loss prefered for training"],
 
