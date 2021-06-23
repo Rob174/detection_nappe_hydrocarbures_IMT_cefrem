9ed402009efade447be046a9caf33a1ffc92ba00
Rob174 <robin.128.456@gmail.com>
2021-06-23 16:06:57 +0100

    Simplification of analyser

--- main/src/analysis/launch_taboo.py
+++ main/src/analysis/launch_taboo.py
@@ -1,7 +1,11 @@
 import tabloo
 import pandas as pd
 
+from main.FolderInfos import FolderInfos
+
 if __name__ == "__main__":
-    path_cache = "C:/Users/robin/Documents/projets/detection_nappe_hydrocarbures_IMT_cefrem/main/src/analysis/data_taboo.pkl"
+    FolderInfos.init(test_without_data=True)
+    l_path = FolderInfos.root_folder.split(FolderInfos.separator) + ["main","src","analysis","data_taboo.pkl"]
+    path_cache = FolderInfos.separator.join(l_path)
     df = pd.read_pickle(path_cache)
     tabloo.show(df)
