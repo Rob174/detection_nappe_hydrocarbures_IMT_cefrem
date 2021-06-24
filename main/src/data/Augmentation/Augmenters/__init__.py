f9ebe0d59947ac459a0835619c23b2d0d03156b0
Rob174 <robin.128.456@gmail.com>
2021-06-24 11:39:48 +0100

    Adding doc

---
+++ main/src/data/Augmentation/Augmenters/__init__.py
@@ -0,0 +1,15 @@
+"""
+Augmenters classes which manage and apply augmentations.
+
+For good practices purposes, these classes have to store informations about which augmentations are applied,
+eventually how these augmentation are applied.... with the attribute attr_ syntax (cf saver class)
+
+They have to provide at least the following interface:
+
+>>> class NoAugmenter(BaseClass):
+...     def __init__(self,*args,**kargs):
+...         # store informations about transformations in attr_... attributes
+...     def transform(self,image: np.ndarray, annotation: np.ndarray) -> Tuple[np.ndarray,np.ndarray]:
+...         # ...Apply the same transformation to the image and annotation...
+...         return image,annotation
+"""
