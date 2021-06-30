70236f4bf8b7b1df3ed776c592b456327984118f
Rob174 <robin.128.456@gmail.com>
2021-06-30 15:19:45 +0100

    Test rotate comparison ok Doc added

--- main/explore/test_cv_rotate.py
+++ main/explore/test_cv_rotate.py
@@ -1,24 +1,63 @@
+"""Script that compares two algorithm to extract a rotate patch of a big image
+Supposition: On my laptop with ~16GB RAM I cannot store 2 times an array of dimensions ( 14600,14600,3)
 
+Input image:
+The rotated image of a white image with a small rectangle (that can be fitted in memory multiple times (> 5))
+
+1st algorithm:
+Approach 1:
+create a transformation array composing a rotation of 45° (translation,rotation,and translation cf issue) with another translation
+and then ask only to compute the result image with a window size of the size of the patch
+
+Approach 2:
+rotate the image
+slice the patch
+
+Difference:
+In approach 2 (on the contrary of approach 1) opencv is required to create a second full size version of the input image
+ which is impossible (cf supposition)
+
+Test procedure:
+- Comment one of the approach section and uncomment the other
+- If the approach 1 is executed no memory must be reported
+- If the approach 2 is executed a memory must be reported
+"""
 from scipy.ndimage import affine_transform
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    img = cv2.imread(r"C:\Users\robin\Documents\projets\detection_nappe_hydrocarbures_IMT_cefrem\data_test\test_shape_characteristics.jpg")
-    # Approach 1
+    img = np.zeros((14600,14600,3),dtype=np.float32)
     rows,cols,_ = img.shape
-    rotation_matrix = cv2.getRotationMatrix2D((rows/2,cols/2),-45,1)
+    shift = 2500
+    size = 100
+    img[shift:shift+size,shift:shift+size,:] = 255
+    transformation_matrix = cv2.getRotationMatrix2D((rows/2,cols/2),-45,1)
+    img = cv2.warpAffine(img,transformation_matrix,dsize=(cols,rows))
+
+    # Approach 1
     translate = np.array([[1,0,rows/2],
                       [0,1,cols/2],
                       [0,0,1]])
-    rotation_centered_matrix = np.concatenate((cv2.getRotationMatrix2D((0,0),-45,1),[[0,0,1]]))
+    rotation_centered_matrix = np.concatenate((cv2.getRotationMatrix2D((0,0),45,1),[[0,0,1]]))
     # Center the desired patch
-    coord_upper_left = np.array([10000.,10000.]).T
-    size = 1000
+    coord_upper_left = np.array([-shift,-shift]).T
     shift_patch = np.identity(3)
-    shift_patch[:-1,-1] = coord_upper_left+size/2
+    shift_patch[:-1,-1] = coord_upper_left
     transformation_matrix = translate.dot(rotation_centered_matrix).dot(shift_patch)[:-1,:]
-    result = cv2.warpAffine(img,transformation_matrix,dsize=(size,)*2)
-    import matplotlib.pyplot as plt
-    plt.imshow(result,cmap="gray")
-    plt.show()
+    transformation_matrix_without_transl = cv2.getRotationMatrix2D((rows / 2, cols / 2), 45, 1)#.dot(shift_patch)[:-1,:]
+    rotation_matr = np.concatenate((cv2.getRotationMatrix2D((rows / 2, cols / 2), 45, 1),[[0,0,1]]),axis=0) #2500,2500
+    transformation_matrix_without_transl1 = shift_patch.dot(rotation_matr)[:-1,:]
+    # result = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # result1 = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # result2 = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # result3 = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # result4 = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # result5 = cv2.warpAffine(img,transformation_matrix_without_transl1,dsize=(size,size))
+    # # # Approach 2
+    transformation_matrix = cv2.getRotationMatrix2D((rows/2,cols/2),45,1)
+    result = cv2.warpAffine(img,transformation_matrix,dsize=(cols,rows))
+    result1 = cv2.warpAffine(img,transformation_matrix,dsize=(cols,rows))
+    result2 = cv2.warpAffine(img,transformation_matrix,dsize=(cols,rows))
+    print()
