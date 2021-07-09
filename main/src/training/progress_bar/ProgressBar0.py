192f055c511e22e5176e6d1a41e2c8624e6c8648
Rob174 <robin.128.456@gmail.com>
2021-07-09 22:22:24 +0100

    Progressbar property bug fixed

--- main/src/training/progress_bar/ProgressBar0.py
+++ main/src/training/progress_bar/ProgressBar0.py
@@ -10,16 +10,17 @@ class ProgressBar0(BaseClass, AbstractProgressBar):
         self.columns.insert(8, TextColumn("[bold blue]num_processed_img: {task.fields[img_processed]:.4e}",
                                           justify="right"))
         self.columns.insert(9, "•")
-        self.progress_bar = Progress(*self.columns)
-        self.progress_bar_epochs = self.progress_bar.add_task("epochs", name="[red]Epochs", loss=0.,
+        columns = self.columns
+        self._progress_bar = Progress(*columns)
+        self._progress_bar_epochs = self._progress_bar.add_task("epochs", name="[red]Epochs", loss=0.,
                                                               total=num_epochs,
                                                               status=0)
-        self.progress_bar_iterations = self.progress_bar.add_task("iterations", name="[blue]Global iterations", loss=0.,
+        self._progress_bar_iterations = self._progress_bar.add_task("iterations", name="[blue]Global iterations", loss=0.,
                                                                   total=length * num_epochs,
                                                                   status=0)
 
     def end_iteration(self, loss: float, it_tr: int, tr_batch_size: int, **kwargs):
-        self.progress_bar.update(self.progress_bar_iterations, advance=tr_batch_size, loss=loss, status=it_tr)
+        self._progress_bar.update(self._progress_bar_iterations, advance=tr_batch_size, loss=loss, status=it_tr)
 
     def end_epoch(self, loss: int, epoch: int, **kwargs):
-        self.progress.update(self.progress_bar_epochs, advance=1, loss=loss, status=epoch)
+        self._progress_bar.update(self._progress_bar_epochs, advance=1, loss=loss, status=epoch)
--- main/src/training/progress_bar/ProgressBar1.py
+++ main/src/training/progress_bar/ProgressBar1.py
@@ -7,14 +7,15 @@ from main.src.training.progress_bar.AbstractProgressBar import AbstractProgressB
 class ProgressBar1(BaseClass, AbstractProgressBar):
     def __init__(self, num_epochs: int):
         super(ProgressBar1, self).__init__()
-        self.progress_bar = Progress(*self.columns)
-        self.progress_bar_epochs = self.progress_bar.add_task("epochs", name="[red]Epochs", loss=0.,
+        columns = self.columns
+        self._progress_bar = Progress(*columns)
+        self._progress_bar_epochs = self._progress_bar.add_task("epochs", name="[red]Epochs", loss=0.,
                                                               total=num_epochs,
                                                               status=0, img_processed=0)
 
     def end_iteration(self, loss: float, epoch: int, img_processed: int, **kwargs):
-        self.progress_bar.update(self.progress_bar_iterations, advance=0, loss=loss, status=epoch,
+        self._progress_bar.update(self._progress_bar_epochs, advance=0, loss=loss, status=epoch,
                                  img_processed=img_processed)
 
     def end_epoch(self, loss: int, epoch: int, img_processed: int, **kwargs):
-        self.progress.update(self.progress_bar_epochs, advance=1, loss=loss, status=epoch, img_processed=img_processed)
+        self._progress_bar.update(self._progress_bar_epochs, advance=1, loss=loss, status=epoch, img_processed=img_processed)
