c4a73c65fe89e9ba587a9b3d9bcfebbceae827b5
Rob174 <robin.128.456@gmail.com>
2021-07-15 13:47:22 +0100

    Correction wrong methods

--- main/src/data/Augmentation/Augmentations/AbstractAugmentationWithMatrix.py
+++ main/src/data/Augmentation/Augmentations/AbstractAugmentationWithMatrix.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, Callable
 
 import numpy as np
 
@@ -8,9 +8,15 @@ from main.src.param_savers.BaseClass import BaseClass
 
 class AbstractAugmentationWithMatrix(ABC, BaseClass):
     @abstractmethod
-    def compute_random_augment(self, image: np.ndarray, annotation: np.ndarray,
+    def compute_image_augment(self, image: np.ndarray,
                                partial_transformation_matrix: np.ndarray,
-                               coord_patch: Tuple[int, int]) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+                               coord_patch: Tuple[int, int]) -> Tuple[np.ndarray, np.ndarray]:
+        """Make the transformation on the image and annotation and returns the results with the affine transformation matrix"""
+        pass
+    @abstractmethod
+    def compute_label_augment(self,annotation_function: Callable,image_name: str,
+                               partial_transformation_matrix: np.ndarray,
+                               coord_patch: Tuple[int, int]) -> Tuple[np.ndarray, np.ndarray]:
         """Make the transformation on the image and annotation and returns the results with the affine transformation matrix"""
         pass
 
