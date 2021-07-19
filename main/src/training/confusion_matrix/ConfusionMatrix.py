5d360883d6164f4af312320c4762d8d84f3d93cc
Rob174 <robin.128.456@gmail.com>
2021-07-19 09:56:09 +0100

    Adding base class for confusion matrix

---
+++ main/src/training/confusion_matrix/ConfusionMatrix.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from typing import Dict, List
+
+from main.src.param_savers.BaseClass import BaseClass
+
+
+class ConfusionMatrix(BaseClass):
+    def __init__(self,class_mappings: Dict[str,str],class_names: List[str]):
+        """
+
+        Args:
+            class_mappings: dict of str,  str with for each name the function to extract on an individual prediction (no batch dim)
+            class_names: names in the order to indicate how to map index to real names
+        """
+        self.attr_class_names = class_names
+        self.attr_class_mappings = class_mappings
+        self.num_matrix_classes = len(self.attr_class_mappings)
+        self.matrix = np.zeros((self.num_matrix_classes,self.num_matrix_classes))
+        self.tot_pred = np.zeros((self.num_matrix_classes,))
+        self.tot_true = np.zeros((self.num_matrix_classes,))
+        self.attr_full_matrix = None
+    def update_matrix(self,prediction,true):
+        """Creates the confusion matrix corresponding to the validation batch
+
+        Args:
+            prediction: np array of shape [batch_size,num_classes]
+            true: np array of shape [batch_size,num_classes]
+
+        Returns:
+
+        """
+        for pred_value,true_value in zip(prediction,true):
+            class_matrix_pred = "none"
+            class_matrix_true = "none"
+            for class_index,function_test in self.attr_class_mappings.items():
+                if eval(function_test)(pred_value):
+                    class_matrix_pred = class_index
+                if eval(function_test)(true_value):
+                    class_matrix_true = class_index
+            if class_matrix_true != "none" and class_matrix_pred != "none":
+                class_matrix_pred = int(class_matrix_pred)
+                class_matrix_true = int(class_matrix_true)
+            self.matrix[class_matrix_pred,class_matrix_true] += 1
+            self.tot_pred[class_matrix_pred] += 1
+            self.tot_true[class_matrix_true] += 1
+
+        full_matrix = np.zeros((self.num_matrix_classes+1,self.num_matrix_classes+1))
+        full_matrix[:self.num_matrix_classes,:self.num_matrix_classes] = self.matrix
+        full_matrix[:self.num_matrix_classes, -1] = self.tot_pred
+        full_matrix[-1, :self.num_matrix_classes] = self.tot_true
+        full_matrix[-1,-1] = np.sum(np.diag(self.matrix))
+
+        tot = np.sum(self.matrix)
+        full_matrix_percent = np.copy(full_matrix) / tot * 100
+
+        final_matrix = np.empty((self.num_matrix_classes+1,self.num_matrix_classes+1)).tolist()
+        for x in range(self.num_matrix_classes+1):
+            for y in range(self.num_matrix_classes+1):
+                final_matrix[x][y] = f"{full_matrix[x,y]}<br>{full_matrix_percent[x,y]:.2f}%"
+        final_matrix.insert(0,[]+self.attr_class_names)
+        for l_id in range(self.num_matrix_classes):
+            final_matrix[l_id+1].insert(0,self.attr_class_names[l_id])
+        self.attr_full_matrix = final_matrix
---
+++ main/test/unit/trainer/test_confusion_matrix.py
@@ -0,0 +1,26 @@
+import unittest
+
+from main.src.training.confusion_matrix.ConfusionMatrix import ConfusionMatrix
+
+import numpy as np
+
+
+class test_confusion_matrix(unittest.TestCase):
+    def test_confusion_matrix_compiles(self):
+        confusion_matrix = ConfusionMatrix(class_mappings={
+            "0":"lambda label:label[0] == 0 and label[1] == 0",
+            "1":"lambda label:label[0] == 0 and label[1] == 1",
+            "2":"lambda label:label[0] == 1 and label[1] == 0",
+            "3":"lambda label:label[0] == 1 and label[1] == 1"
+        },
+            class_names=[
+                "Nothing",
+                "Spill",
+                "Seep",
+                "SeepSpill"
+            ]
+        )
+        true = np.random.randint(0,2,(100,2))
+        pred = np.random.randint(0, 2, (100, 2))
+        confusion_matrix.update_matrix(pred,true)
+        print("\n".join(list(map(lambda x:"\t".join(x),confusion_matrix.attr_full_matrix))))
