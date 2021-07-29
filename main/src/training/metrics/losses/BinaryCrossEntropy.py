820759b256f91aaec2027aed42e30995dbc39535
Rob174 <robin.128.456@gmail.com>
2021-07-29 14:29:48 +0100

    Correction losses pytorch usage

--- main/src/training/metrics/losses/BinaryCrossEntropy.py
+++ main/src/training/metrics/losses/BinaryCrossEntropy.py
@@ -9,8 +9,9 @@ class BinaryCrossEntropy(BaseClass, AbstractLoss):
     def __init__(self,optimizer):
         super(BinaryCrossEntropy, self).__init__(optimizer)
         self.attr_name = self.__class__.__name__
+        self.loss = torch.nn.BCELoss()
     def torch_compute(self, true_batch, pred_batch):
-        return torch.nn.BCELoss()
+        return self.loss(pred_batch,true_batch)
 
     def npy_compute(self, true_batch, pred_batch):
         return np.mean(-(true_batch * np.log(np.clip(pred_batch, np.exp(-100), None)) + (1 - true_batch) * np.log(
--- main/src/training/metrics/losses/MSE.py
+++ main/src/training/metrics/losses/MSE.py
@@ -9,8 +9,9 @@ class MSE(BaseClass, AbstractLoss):
     def __init__(self,optimizer):
         super(MSE, self).__init__(optimizer)
         self.attr_name = self.__class__.__name__
+        self.loss = torch.nn.MSELoss()
     def torch_compute(self, true_batch, pred_batch):
-        return torch.nn.MSELoss()
+        return self.loss(pred_batch,true_batch)
 
     def npy_compute(self, true_batch, pred_batch):
         return np.mean(np.power(pred_batch - true_batch, 2))
